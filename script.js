516003d318b2e8354a3c2249a8725c7c59171ae6
Pushparaj1381 <pushparajmehta002@gmail.com>
2023-03-12 19:02:23 +0530

    Updated all
    All the code and files are updated

--- script.js
+++ script.js
@@ -1,30 +1,65 @@
-let header = document.querySelector("header");
-let menu = document.querySelector("#menu-icon");
+/*=============== toggle icon navbar ================*/
+let menuIcon = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 
-window.addEventListener("scroll", () => {
-  header.classList.toggle("shadow", window.scrollY > 0);
-});
-
-menu.onclick = () => {
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
   navbar.classList.toggle("active");
 };
+
+/*=============== scroll section active links ================*/
+let sections = document.querySelectorAll("section");
+let navlinks = document.querySelectorAll("header nav a");
+
 window.onscroll = () => {
-  navbar.classList.remove("active");
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navlinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+
+  /*=============== sticky navbar ================*/
+  let header = document.querySelector("header");
+
+  header.classList.toggle("sticky", window.scrollY > 100);
+
+  /*=============== remove toggle icon and navbar when navbar link is clicked(scroll)================*/
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
 };
 
-// Dark Mode / light mode
-let darkmode = document.querySelector("#darkmode");
+/*=============== scroll revel================*/
+ScrollReveal({
+  reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
 
-darkmode.onclick =() => {
-  if(darkmode.classList.contains("bx-moon")){
-    darkmode.classList.replace("bx-moon", "bx-sun");
-    document.body.classList.add("active");
-  } 
-  else {
-    darkmode.classList.replace("bx-sun", "bx-moon");
-    document.body.classList.remove("active");
-  }
-}
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+ScrollReveal().reveal(
+  '.home-img, .services-container, .portfolio-box, .contact form',
+  {
+    origin: "bottom",
+    });
+ScrollReveal().reveal(
+  '.home-content h1, .about-img ',
+  {
+    origin: "left",
+    });
+ScrollReveal().reveal(
+  '.home-content p, .about-content ',
+  {
+    origin: "right",
+    });
 
-                                  
